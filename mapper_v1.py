d1445c362e7b12afeba65f4a4839230ce751c482
Cameron Bayer <138840395+CameronB01@users.noreply.github.com>
2024-06-05 20:59:17 -0700

    Update mapper_v1.py

--- mapper_v1.py
+++ mapper_v1.py
@@ -1,20 +1,5 @@
 #!/usr/bin/python3
 # -*-coding:utf-8 -*
-# import sys 
-# import nltk
-# nltk.download('stopwords')
-# from nltk.corpus import stopwords
-# en_stopwords = stopwords.words('english')
-
-  
-# for line in sys.stdin: 
-#     lower = line.apply(lambda x: str(x).lower())
-#     lower = ' '.join([word for word in lower.split() if word not in en_stopwords])
-#     words = lower.str.strip().str.replace(r'[^\w\s]', '', regex=True)
-#     for word in words:
-#         print('%s\t%s' % (word, 1))
-
-
 import sys
 import re
 
