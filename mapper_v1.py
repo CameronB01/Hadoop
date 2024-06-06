134c2180641233c20ee37f6a6a0ce722d008cb6d
Cameron Bayer <138840395+CameronB01@users.noreply.github.com>
2024-06-05 20:55:03 -0700

    Update mapper_v1.py

--- mapper_v1.py
+++ mapper_v1.py
@@ -22,4 +22,6 @@ for line in sys.stdin:
     line = line.strip()
     words = re.findall(r'[a-zA-Z]+', line)
     for word in words:
+        word = word.lower()
         print('%s\t%s' % (word, 1))
+
